a15e7cba44d81cf9d70c7ed6d9c09f202cb66a55
BelovEA <vrachea676@gmail.com>
2022-12-21 06:37:00 +0300

    start my proj

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+
+
+
+
+class Logger:
+    day = 0
+    month = 0
+    year = 0
+    hour = 0
+    minute = 0
+    second = 0
+    path = ''
+
+
+
+    def __init__(self, path="./"):
+        Logger.fill_date()
+        Logger.path = path
+        with open(f"{path}log_{Logger.day}.{Logger.month}.{Logger.year}.log", "w"):
+            pass
+
+
+    @classmethod
+    def fill_date(self):
+        current_date = datetime.datetime.now()
+        Logger.day = current_date.day
+        Logger.month = current_date.month
+        Logger.year = current_date.year
+        Logger.hour = current_date.hour
+        Logger.minute = current_date.minute
+        Logger.second = current_date.second
